c7e33d2776740b1e8fb1aa27a32bb6905e349baa
Randall Balestriero <rbalestriero@fb.com>
2022-08-03 07:38:23 -0700

    test

--- mnist_experiments.py
+++ mnist_experiments.py
@@ -1,5 +1,15 @@
 """
 File: mnist_experiments.py
+Project: torchstrap
+-----
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+"""
+
+"""
+File: mnist_experiments.py
 Created Date: Wed Mar 09 2022
 Author: Randall Balestriero
 -----
@@ -20,20 +30,33 @@ import os
 
 def load_data(name):
 
-    # load data
+    if "CIFAR" in name:
+        stats = ((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+        train_tfms = transforms.Compose(
+            [
+                transforms.RandomCrop(32, padding=4, padding_mode="reflect"),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(*stats, inplace=True),
+            ]
+        )
+        valid_tfms = transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize(*stats)]
+        )
+    else:
+        train_tfms = transforms.ToTensor()
+        valid_tfms = transforms.ToTensor()
     train = torchvision.datasets.__dict__[name](
-        "./", download=True, train=True, transform=transforms.ToTensor()
+        "./", download=True, train=True, transform=train_tfms
     )
-    train = torch.utils.data.random_split(train, [args.ds, len(train) - args.ds])[0]
-
     test = torchvision.datasets.__dict__[name](
-        "./", download=True, train=False, transform=transforms.ToTensor()
+        "./", download=True, train=False, transform=valid_tfms
     )
     train_loader = torch.utils.data.DataLoader(
         train, batch_size=args.bs, shuffle=True, drop_last=True
     )
     test_loader = torch.utils.data.DataLoader(
-        test, batch_size=args.bs, shuffle=False, drop_last=False
+        test, batch_size=args.bs, shuffle=False, drop_last=True
     )
 
     if name == "MNIST":
@@ -52,28 +75,38 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-lr", type=float, default=1.0)
 parser.add_argument("-lr_divider", type=float, default=10.0)
 parser.add_argument("-bs", type=int, default=256)
-parser.add_argument("-dname", type=str, default="CIFAR10")
-parser.add_argument("-ds", type=int, default=50000)
+parser.add_argument(
+    "--optimizer", type=str, default="RMSProp", choices=["RMSProp", "SGD"]
+)
+parser.add_argument(
+    "--dataset", type=str, default="CIFAR10", choices=["MNIST", "CIFAR10", "CIFAR100"]
+)
 parser.add_argument("-gamma", type=float, default=0.0)
-parser.add_argument("-u", action="store_true")
+parser.add_argument("--unitary", action="store_true")
 args = parser.parse_args()
 
 
-train_loader, test_loader, epochs, channels, length = load_data(args.dname)
+train_loader, test_loader, epochs, channels, length = load_data(args.dataset)
 model = projunn.models.ResNet9(
-    channels, num_classes=10, image_length=length, unitary=args.u
+    channels, num_classes=10, image_length=length, unitary=args.unitary
 ).cuda()
 model.train()
 
 
+# create projector and optionally the optimizer
+def projector(param, update):
+    a, b = projunn.utils.LSI_approximation(update, k=1)
+    update = projunn.utils.projUNN_T(param.data, a, b, project_on=False)
+    return update
+
+
+if args.optimizer == "RMSProp":
+    optimizer = projunn.optimizers.RMSprop(
+        model.parameters(), projector=projector, lr=args.lr
+    )
+
 accuracy = torchmetrics.Accuracy(compute_on_step=False).cuda()
 losses, accuracies = [], []
-filename = f"logger_{args.dname}_{args.u}_{args.lr}_{args.lr_divider}_{args.bs}_{args.ds}_{args.gamma}.npz"
-
-if os.path.exists(filename):
-    import sys
-
-    sys.exit()
 
 if args.gamma:
     regularizer = projunn.utils.OrthoRegularizer(model, [3, 3])
@@ -93,23 +126,26 @@ for epoch in range(epochs):
 
         model.zero_grad()
         loss.backward()
-
+        if args.optimizer == "RMSProp":
+            optimizer.step()
+        else:
+            for param in model.parameters():
+                update = -args.lr * param.grad
+                if hasattr(param, "needs_projection"):
+                    update = projector(update)
+                param.data.add_(update)
         with torch.no_grad():
             accuracy(prediction, labels.cuda())
             if iter % 10 == 0:
                 losses.append(loss.item())
-                print(f"Current loss is: {losses[-1]}")
-            for param in model.parameters():
-                update = -args.lr * param.grad
-                if hasattr(param, "needs_projection"):
-                    a, b = projunn.utils.LSI_approximation(
-                        update / args.lr_divider, k=1
-                    )
-                    update = projunn.utils.projUNN_T(param.data, a, b, project_on=True)
-                    param.data.copy_(update)
-                else:
-                    param.data.add_(update)
-    acc = accuracy.compute().item()
+                l1_weight = model.conv1[0].weight
+                diff = l1_weight @ torch.conj(l1_weight.permute(0, 2, 1)) - torch.eye(
+                    l1_weight.shape[1], device="cuda"
+                )
+                print(
+                    f"Current loss is: {losses[-1]}, and {torch.sum(diff*diff.conj())}"
+                )
+
     accuracy.reset()
     accuracies.append(acc)
     print("epoch train accuracy", acc)
